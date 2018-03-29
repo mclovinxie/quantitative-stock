076cb207305111d4eb2d1502fefb318097618362
unknown <xiexihao@we.com>
2018-03-29 19:51:53 +0800

    update

--- pys/Crawler.py
+++ pys/Crawler.py
@@ -12,6 +12,7 @@ import os
 import time
 
 import DBUtil as dbu
+import IndexUtil as iu
 
 URL = 'http://quote.eastmoney.com/stocklist.html'
 DataDir = './../data/crawl/'
@@ -113,3 +114,76 @@ def crawl(mode = DefaultCrawlMode):
     store2DB()
     
     dbu.closeDBCon()
+
+
+def updateIndex(mode = DefaultCrawlMode):
+    if dbu.DBCon == None:
+        dbu.initDBCon()
+    
+    cursor = dbu.DBCon.cursor()
+    
+    stockCodeList = []
+    
+    try:
+        crawledCodes = getStockCode(getHtml(URL))
+        for code in crawledCodes:
+            stockCodeList.append(code)
+    except:
+        sql = "SELECT DISTINCT(stock_code) FROM " + dbu.DBName + ".stock_price_data"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for row in result:
+            stockCodeList.append(row[0])
+    
+    currentDate = time.strftime('%Y%m%d', time.localtime(time.time()))
+    
+    if mode == 'ALL':
+        startDate = DefaultStartDate
+        #sql = "DROP DATABASE IF EXISTS " + dbu.DBName
+        #cursor.execute(sql)
+        #sql = "CREATE DATABASE " + dbu.DBName
+        #cursor.execute(sql)
+        sql = "USE " + dbu.DBName
+        cursor.execute(sql)
+        sql = "DROP TABLE IF EXISTS " + dbu.DBName + ".stock_index_data"
+        cursor.execute(sql)
+        sql = "CREATE TABLE stock_index_data (data_date date, stock_code VARCHAR(10), ra5 float, ra10 float, ra20 float, ra30 float, dif float, dea float, macd float, PRIMARY KEY (data_date, stock_code), INDEX(data_date), INDEX(stock_code))"
+        cursor.execute(sql)
+    else:
+        sql = "USE " + dbu.DBName
+        cursor.execute(sql)
+        sql = "SELECT MAX(data_date) FROM stock_price_data"
+        cursor.execute(sql)
+        dt = cursor.fetchone()
+        startDate = dt[0].strftime('%Y%m%d')
+        if startDate >= currentDate:
+            return
+    
+    for code in stockCodeList:
+        print('正在生成股票[%s]的指标数据...' % code)
+        time.sleep(0.25)
+        indexdata = iu.indexCal(code)
+        #if indexdata is None:
+        #    continue
+        #length = len(indexdata)
+        #for i in range(0, length):
+            #record = indexdata.loc[i]
+        for record in indexdata:
+            #插入数据语句
+            try:
+                sql = "INSERT INTO stock_index_data (data_date, stock_code, ra5, ra10, ra20, ra30, dif, dea, macd) values ('%s','%s',%f,%f,%f,%f,%f,%f,%f)" % (record['data_date'], code, record['ra5'], record['ra10'], record['ra20'], record['ra30'], record['dif'], record['dea'], record['macd'])
+                #获取的表中数据很乱，包含缺失值、Nnone、none等，插入数据库需要处理成空值
+                sql = sql.replace('nan','null').replace('None','null').replace('none','null') 
+                cursor.execute(sql)
+            except:
+                #如果以上插入过程出错，跳过这条数据记录，继续往下进行
+                break
+    
+    #关闭游标，提交，关闭数据库连接
+    cursor.close()
+    dbu.DBCon.commit()
+    #con.close()
+
+
+if __name__ == '__main__':
+    updateIndex('ALL')
--- pys/IndexUtil.py
+++ pys/IndexUtil.py
@@ -5,38 +5,39 @@ Created on Wed Mar 28 19:27:55 2018
 @author: xiexihao
 """
 
-import datetime
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime as dttm
 
 import DataReader as dr
 
-def smaCal(tsPrice,k):
-    Sma=pd.Series(0.0,index=tsPrice.index)
-    for i in range(k-1,len(tsPrice)):
-        Sma[i]=sum(tsPrice[(i-k+1):(i+1)])/k
+def smaCal(tsPrice, k):
+    Sma = pd.Series(0.0, index = tsPrice.index)
+    for i in range(k - 1,len(tsPrice)):
+        Sma[i] = sum(tsPrice[(i - k + 1) : (i + 1)]) / k
     return(Sma)
 
-def wmaCal(tsPrice,weight):
-   k=len(weight)
-   arrWeight=np.array(weight)
-   Wma=pd.Series(0.0,index=tsPrice.index)
-   for i in range(k-1,len(tsPrice.index)):
-       Wma[i]=sum(arrWeight*tsPrice[(i-k+1):(i+1)])
+def wmaCal(tsPrice, weight):
+   k = len(weight)
+   arrWeight = np.array(weight)
+   Wma = pd.Series(0.0, index = tsPrice.index)
+   for i in range(k - 1,len(tsPrice.index)):
+       Wma[i] = sum(arrWeight * tsPrice[(i - k + 1) : (i + 1)])
    return(Wma)
 
-def ewmaCal(tsprice,period=5,exponential=0.2):
-   Ewma=pd.Series(0.0,index=tsprice.index)
-   Ewma[period-1]=np.mean(tsprice[0:period])
-   for i in range(period,len(tsprice)):
-       Ewma[i]=exponential*tsprice[i]+(1-exponential)*Ewma[i-1]
+def ewmaCal(tsprice, period = 5, exponential = 0.2):
+   Ewma = pd.Series(0.0, index = tsprice.index)
+   Ewma[period - 1] = np.mean(tsprice[0 : period])
+   for i in range(period, len(tsprice)):
+       Ewma[i] = exponential * tsprice[i] + (1 - exponential) * Ewma[i - 1]
    return(Ewma)
 
-def getMACD(code):
-    data = dr.readStockData(code, '2000-01-01', '2018-03-28')
+def getRollingMean(close, window = 5):
+    return close.rolling(window = window).mean()
+
+def indexCal(code):
+    data = dr.readStockData(code, '1900-01-01', '2018-03-28')
     data.sort_values('data_date', inplace = True)
     
     data.index = data.data_date
@@ -49,6 +50,11 @@ def getMACD(code):
     
     close = data.close
     
+    indexdata = pd.DataFrame()
+    
+    if len(close) <= 12:
+        return indexdata
+    
     dif = ewmaCal(close, 12, 2 / (1 + 12)) - ewmaCal(close, 26, 2 / (1 + 26))
     #dif.tail(n = 3)
     
@@ -58,8 +64,9 @@ def getMACD(code):
     macd = dif - dea
     #macd.tail(n = 3)
     
-    print(macd)
+    #print(macd)
     
+    '''
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.subplot(211)
     plt.plot(dif,label="DIF",color='k')
@@ -71,11 +78,22 @@ def getMACD(code):
             height=macd,\
             label='MACD',color='r')
     plt.legend()
+    '''
     
-    macddata=pd.DataFrame()
-    macddata['dif']= dif
-    macddata['dea']= dea
-    macddata['macd']= macd
+    
+    indexdata['ra5'] = close.rolling(window = 5).mean()
+    indexdata['ra10'] = close.rolling(window = 10).mean()
+    indexdata['ra20'] = close.rolling(window = 20).mean()
+    indexdata['ra30'] = close.rolling(window = 30).mean()
+    indexdata['dif']= dif
+    indexdata['dea']= dea
+    indexdata['macd']= macd
+    indexdata['data_date'] = macd.index
+    indexdata.index = indexdata.data_date
+    
+    #print(indexdata)
+    
+    return indexdata
 
 if __name__ == '__main__':
-    getMACD('603118')
+    indexCal('603118')
